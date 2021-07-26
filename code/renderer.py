397455a0c76eb9357c1436c7ad7149d221b5ef0d
Dennis <bouncy.lu@hotmail.com>
2021-07-26 22:41:25 +0800

    Change render scale

--- code/renderer.py
+++ code/renderer.py
@@ -4,9 +4,9 @@ import torch
 
 
 # Adapted from https://github.com/Oktosha/DeepSDF-explained/blob/master/deepSDF-explained.ipynb
-def plot_sdf(sdf_func, device, filepath='', filename='res.png', is_net=False):
+def plot_sdf(sdf_func, device, size=800, filepath='', filename='res.png', is_net=False):
     # Sample the 2D domain as a regular grid
-    coordinates_linspace = np.linspace(-4, 4, 100)
+    coordinates_linspace = np.linspace(-4, 4, 101)
     y, x = np.meshgrid(coordinates_linspace, coordinates_linspace)
     if not is_net:
         z = [[sdf_func(np.float_([x_, y_]))
@@ -18,14 +18,14 @@ def plot_sdf(sdf_func, device, filepath='', filename='res.png', is_net=False):
              for x_ in coordinates_linspace]
 
     z = np.float_(z)
-
     z = z[:-1, :-1]
 
-    # TODO: use color to differentiate negative and positive
+    scale = int(size / 100)
+    z = np.kron(z, np.ones((scale, scale)))
+
     # z_min, z_max = -np.abs(z).max(), np.abs(z).max()
     # z = np.abs(z) / z_max * 255
     z = 1 / (1 + np.exp(-20 * z)) * 255
-
     z = np.uint8(z)
 
     cv2.imwrite(f'{filepath}{filename}', z)
